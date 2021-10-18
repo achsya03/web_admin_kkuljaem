631567f908eedf3d8fb141da5bd681cff0849885
kowecopo <adi.wahyu87@gmail.com>
2021-10-18 07:50:16 +0700

    FCK U DOG

--- resources/views/keuangan/pembayaran/detail_transaksi.blade.php
+++ resources/views/keuangan/pembayaran/detail_transaksi.blade.php
@@ -1,12 +1,13 @@
 @section('title', 'Detail Transaksi')
 @section('title-description', 'Memantau transaksi dan keuangan aplikasi')
 @section('title-icon', 'pe-7s-bookmarks')
-
+@section('content')
 <div class="col-lg-13">
     <div class="main-card mb-3 card">
         <div class="card-body">
             <h4 style="color:black"><strong>Informasi Pembayaran</strong></h4>
-            nandamohammad@gmail.com <br>
+            <a id=""> nandamohammad@gmail.com </a>
+            <br>
 
             <span style="color:black"><strong>Status</strong></span><br>Member
             <br>
@@ -21,8 +22,6 @@
 
             <br>
             <br>
-            <a>Pin telang dipasang </a><a href="">Lihat Peta</a>
-
             <br>
         </div>
     </div>
@@ -49,5 +48,31 @@
 
 
 @endsection
+@section('js')
 
+<script>
+    function load_payment() {
+        $('#cover-spin').show();
+        $.ajax({
+            "url": api + "admin/subs/detail?token=" + urlParams.get('id'),
+            "method": "GET",
+            "headers": {
+                "Accept": "application/json",
+                "Authorization": 'bearer ' + token,
+            },
+        }).done(function(response) {
+            $('#cover-spin').hide();
+            if (response.message == 'Success') {
+                console.log(response)
+                document.getElementById('saldo').textContent = response.data['total_saldo'];
+                document.getElementById('jumlah_t').textContent = response.data['jml_transaksi'];
+
+   
+            }
+        });
+    }
+    load_payment();
+</script>
+
+@endsection
 @extends('layouts.layout')
--- resources/views/keuangan/penawaran/index.blade.php
+++ resources/views/keuangan/penawaran/index.blade.php
@@ -3,33 +3,13 @@
 @section('title-icon', 'pe-7s-bookmarks')
 @section('content')
 
-<div class="col-md-4">
+<!-- <div class="col-md-4">
     <div class="main-card mb-3 card">
         <div class="card-body">
-            <a>
                 <div class="link">
                     <h5><strong><u>12 Bulan</strong> (Rp. 20.000)</h5></u>
                 </div>
-            </a>
             <div class="position-relative form-group"><a data-toggle="modal" data-target=".bd-example-modal-sm" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav" href=""> Edit</a></div>
-            <br>
-            <br>
-            <br>
-        </div>
-    </div>
-</div>
-<div class="col-md-4">
-    <div class="main-card mb-3 card">
-        <div class="card-body">
-            <a>
-                <div class="link">
-                    <h5 href=""><strong><u>8 Bulan</strong>(Rp. 20.000</h5></u>
-                </div>
-            </a>
-            <div class="position-relative form-group"><a data-toggle="modal" data-target=".bd-example-modal-sm" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav" href=""> Edit</a></div>
-            <br>
-            <br>
-            <br>
         </div>
     </div>
 </div>
@@ -49,6 +29,18 @@
             <div class="position-relative form-group"> <button type="button" class="btn mr-2 mb-2 btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Tambahkan</button></div>
         </div>
     </div>
+</div> -->
+
+<div>
+    <div class="form-row cards">
+       
+    </div>
+    <div class="form-row cardss">
+
+    </div>
+</div>
+<div class="toast" role="alert" aria-live="polite" aria-atomic="true" data-delay="100000">
+        <div role="alert" aria-live="assertive" aria-atomic="true">...</div>
 </div>
 @endsection
 @section('modal')
@@ -63,14 +55,20 @@
             </div>
             <div class="modal-body">
                 <div class="position-relative form-group">
-                    <div class="position-relative form-group"><label for="exampleSelect" class="">Pilih bank tujuan</label>
-                        <select name="select" id="exampleSelect" class="form-control">
-                            <option>1</option>
-                            <option>2</option>
+                    <div class="position-relative form-group"><label for="exampleSelect" class="">Pilih Penawaran</label>
+                        <select name="lama_paket" id="lama_paket" class="form-control">
+                            <option value="1">1 Bulan</option>
+                            <option value="3">3 Bulan</option>
+                            <option value="6">6 Bulan</option>
+                            <option value="12">12 Bulan</option>
                         </select>
                     </div>
                 </div>
-                <div class="position-relative form-group"><label for="exampleEmail" class="">Harga Penawaran</label><input name="email" id="exampleEmail" placeholder="Masukkan Keterangan singkat disini" type="email" class="form-control"></div>
+                <div class="position-relative form-group"><label for="exampleEmail" class="">Harga Penawaran</label>
+                <input name="harga" id="harga" placeholder="Masukkan Keterangan singkat disini" class="form-control">
+                <input name="status_aktif" id="status_aktif" value="1" hidden placeholder="Masukkan Keterangan singkat disini"  class="form-control">
+
+            </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -82,6 +80,125 @@
 @endsection
 
 @section('js')
+
+<script>
+    function load_penawaran() {
+        $('#cover-spin').show();
+        $.ajax({
+            "url": api + "admin/packet",
+            "method": "GET",
+            "headers": {
+                "Accept": "application/json",
+                "Authorization": 'bearer ' + token,
+            },
+        }).done(function(response) {
+            $('#cover-spin').hide();
+            if (response.message == 'Success') {
+
+                console.log(response)
+
+                html = '';
+                    $.each(response.data, function(index, row) {
+                        html += '<div class="col-md-4">';
+                        html += '<div class = "main-card mb-3 card" >';
+                        html += '<div class = "card-body" >';
+                        html += `<a href="#"  class="text-dark"><h5><strong><u>` + row.lama_paket + '&nbsp'+ 'Bulan'+ '</strong></u>' + '(' + row.harga + ')' + ' </h5>';
+                        html += '<div class="position-relative form-row"><div class="col-md-1"><a data-toggle="modal" data-target=".bd-example-modal-sm" class="btn-icon  btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav" href="#" onclick="getEdit(\'' + row.uuid + '\')">Edit</a></div><div style="text-align:right;" class="col-md-11"><a class="btn-icon  btn-icon-only btn btn-danger btn-sm" href="#" onclick="hapus(`' + api + 'admin/packet?token=' + row.uuid + '`)">Hapus</a></div></div>';
+                        html += '</div>';
+                        html += '</div>';
+                        html += '</div>';
+                        html += '</div>';
+                    });
+                    document.querySelector('.cards').innerHTML = html;
+
+                    html1 = '';
+                    html1 += `<div class="col-md-4"><div class="main-card mb-3 card"><div class="card-body"><h5><strong>Tambah Penawaran</strong></h5><form id="change-pass-form"><div class="position-relative form-group"><label class="">Pilih Penawaran</label><select name="lama_paket" id="exampleSelect" class="form-control"><option value="1">1 Bulan</option><option value="3">3 Bulan</option><option value="6">6 Bulan</option><option value="10">10 Bulan</option></select></div><div class="position-relative form-group"><label for="exampleEmail" class="">Harga Penawaran</label><input name="harga" placeholder="" class="form-control"><input name="status_aktif" value="1" placeholder="" hidden class="form-control"></div><div class="position-relative form-group"><button type="submit" class="btn-icon btn-focus btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">Tambahkan Penawaran</button></div></form></div></div></div>`;
+                    document.querySelector('.cardss').innerHTML = html1;
+   
+                    //CREATE
+                    $('#change-pass-form').submit(function(e) {
+                        e.preventDefault();
+                        $.ajax({
+                            method: 'post',
+                            url: api + 'admin/packet',
+                            data: $('form').serialize(),
+                            dataType: 'json',
+                            headers: {
+                            "Accept": "application/json",
+                            "Authorization": 'bearer ' + token,
+                        },
+                            success: function(response) {
+                                if (response.message !== 'Success') {
+                                    notif('error', 'Mohon semua form diisi !');
+
+                                } else if (response.message == 'Success') {
+                                    notif('success', 'Berhasil membuat grup kelas, Mohon tunggu');
+                                    window.location = "{{ route('penawaran')}}";
+
+                                }
+                            }
+                        });
+                    }); 
+
+            }
+        });
+    }
+    load_penawaran();
+
+
+        //SHOW EDIT
+        function getEdit(id) {
+        $.ajax({
+            method: 'get',
+            url: api +'admin/packet/detail?token=' + id,
+            dataType: 'json',
+            headers: {
+                "Accept": "application/json",
+                "Authorization": 'bearer ' + token,
+            },
+            success: function(response) {
+                if (response.message !== 'Success') {
+                    // $.growl.warning({
+                    //     message: response.message
+                    // });
+                } else if (response.message == 'Success') {
+                    document.getElementById('lama_paket').value = response.data['lama_paket'];
+                    document.getElementById('harga').value = response.data['harga'];
+                    document.getElementById('status_aktif').value = response.data['status_aktif'];
+                }
+            }
+        });
+
+    }
+
+    //UPDATE
+    function update(id) {
+        $.ajax({
+            type: "post",
+            url: api + 'admin/classroom-group/update?token=' + id,
+            data: {
+                'harga': $("#harga").val(),
+                'lama_paket': $("#lama_paket").val(),
+                'status_aktif': $("#status_aktif").val(),
+            },
+            headers: {
+                "Accept": "application/json",
+                "Authorization": 'bearer ' + token,
+            },
+            success: function(response) {
+                if (response.message !== 'Success') {
+                    notif('error', 'Gagal merubah kelas, Silahkan semua isian!');
+
+                } else if (response.message == 'Success') {
+                    notif('success', 'Berhasil merubah kelas, Mohon tunggu');
+                    $(".btn-close").click();
+                    window.location = "{{ route('penawaran')}}";
+                }
+
+            }
+        });
+    }
+</script>
 
 @endsection
 @extends('layouts.layout')
