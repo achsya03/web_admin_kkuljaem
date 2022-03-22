a66320a57c554709cd8e78f0f1b5973b008c755b
kowecopo <adi.wahyu87@gmail.com>
2022-03-22 08:54:01 +0700

    Bismillah change siswa

--- resources/views/akun_pengguna/siswa/index.blade.php
+++ resources/views/akun_pengguna/siswa/index.blade.php
@@ -18,6 +18,7 @@
                 <th>Jenis Kelamin</th>
                 <th>Tempat Lahir</th>
                 <th>Tanggal Lahir</th>
+                <th>Alamat</th>
                 <th>Action</th>
                 </tr>
             </thead>
@@ -35,41 +36,61 @@
 
 <script>
     function load_student() {
-        $('#cover-spin').show();
-        $.ajax({
-            "url": api + "admin/user/student/list",
-            "method": "GET",
-            "headers": {
-                "Accept": "application/json",
-                "Authorization": 'bearer ' + token,
-            },
-        }).done(function(response) {
-            console.log(response)
-            $('#cover-spin').hide();
-            if (response.message == 'Success') {
-                console.log(response)
-                html = '';
-                $.each(response.data, function(index, row) {
-                    html += '<tr>';
-                    html += '<td>' + (index + 1) + '</td>';
-                    html += '<td>' + row.status + '</td>';
-                    html += '<td>' + row.email + '</td>';
-                    html += '<td>' + row.nama + '</td>';
-                    html += '<td>' + row.jenis_kel + '</td>';
-                    html += '<td>' + row.tempat_lahir + '</td>';
+        $(function() {
+            var table = $('.table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    "url": "https://api.kkuljaemkoreanapp.com/api/admin/user/student/lists",
+                    "dataType": 'json',
+                    "type": "GET",
+                    "beforeSend": function(xhr) {
+                        xhr.setRequestHeader("Authorization", "Bearer " + token);
+                    },
+                },
+                columns: [{
+                        data: null,
+                        sortable: false,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    }, {
+                        data: 'status_aktif',
+                        name: 'u.status_aktif'
+                    }, {
+                        data: 'email',
+                        name: 'u.email'
+                    }, {
+                        data: 'nama',
+                        name: 'u.nama'
+                    }, {
+                        data: 'jenis_kel',
+                        name: 'jenis_kel'
+                    }, {
+                        data: 'tgl_lahir',
+                        name: 'tgl_lahir'
+                    }, {
+                        data: 'tempat_lahir',
+                        name: 'tempat_lahir'
+                    }, {
+                        data: 'alamat',
+                        name: 'alamat'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: true,
+                        searchable: true
+                    },
+                ]
+            });
 
-                    html += '<td>' + row.tgl_lahir + '</td>';
-                    html += '<td>' + `<a href="{{route('detailsiswa')}}?id=` + row.user_uuid + `" style="margin:2px;"  type="button" class="btn btn-primary btn-sm">Rincian</a><br>` + `<a href="{{route('editsiswa')}}?token=` + row.user_uuid + `" style="margin:2px;"  type="button" class="btn btn-info btn-sm">Edit</a><br>` + `<a href="#" onclick="hapus('` + api + `admin/user/student?token=` + row.user_uuid + `')" style="margin:2px;"  type="button" class="btn btn-danger btn-sm">Hapus</a>` + '</td>';
-                    html += '</tr>';
-                });
-                document.querySelector('.tbody').innerHTML = html;
-                $('table').DataTable();
-
-            }
         });
     }
     load_student();
 </script>
+
+
 
 @endsection
 @extends('layouts.layout')
